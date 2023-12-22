7ba346e7bb8f23ce08ff4437a727f28d546b06de
mehDeepak <154570491+mehDeepak@users.noreply.github.com>
2023-12-22 14:26:22 +0000

    02 prac of js

---
+++ jsprac/02_prac.js
@@ -0,0 +1,23 @@
+//*********** Some JavaScript method ***************
+
+let myStr = new String("nepal");
+const spaceStr = new String("    my name     ");
+const newStr = new String("NEPAL");
+// console.log(myStr.toUpperCase());
+//console.log(newStr.toLowerCase());
+// console.log(spaceStr);
+// console.log(spaceStr.trim());
+// console.log(myStr.indexOf('n'));
+// console.log(myStr.valueOf());
+// console.log(myStr.slice(0,3));
+// console.log(myStr.replace('n','m'));
+// console.log(myStr.includes('n'));
+// console.log(myStr.endsWith('o'));
+const mood = 'Happy! ';
+console.log(`I feel ${mood.repeat(3)}`);
+
+
+
+
+
+
