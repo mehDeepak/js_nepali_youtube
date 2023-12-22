a5fc0fdebf27a561c7972dbe6deebc6a15f57663
mehDeepak <154570491+mehDeepak@users.noreply.github.com>
2023-12-22 13:42:59 +0000

    container config

---
+++ jsprac/01_prac.js
@@ -0,0 +1,36 @@
+// console.log("hellow");
+let num = 5
+let anotherNum = 10 
+let sumOfNum = num + anotherNum;
+// console.log(sumOfNum);
+let myName = "Deepak";
+let myCaste = " Bist";
+const myAge = 30;
+let fullName = myName + myCaste;
+// console.log(fullName);
+// console.log(`my name is ${fullName} and i am ${myAge} years old`);
+// console.log(typeof(myCaste));
+// console.log(typeof myAge);
+const isLogedIn = true;
+// console.log(typeof(isLogedIn));
+let myFolder;   //undefiend data type 
+console.log(typeof(myFolder));
+let banana = null;
+// console.log(typeof(null));
+//mathmetical operator
+console.log(2+2);
+console.log(8-2);
+console.log(2*2);
+console.log(2**4);
+console.log(10%4);
+
+//relational operator
+console.log(2==2);
+console.log(7>2);
+console.log(2<7)
+console.log(8!=8);
+
+
+
+
+
